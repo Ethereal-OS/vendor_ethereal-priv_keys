ItsVixano/android_vendor_lineage-priv_keys

keys: Include custom releasekey in recovery
Change-Id: I3335af3b490ef1af5fdd786654f4e68bb3290b27

keys.mk
@@ -101,3 +101,4 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     CtsCarrierApiTestCases:cts_uicc_2021.certificate.override
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
+PRODUCT_EXTRA_RECOVERY_KEYS += vendor/lineage-priv/keys/releasekey

Ethereal-OS/vendor_ethereal-priv_keys

Signed-off-by: Dmitrii <bankersenator@gmail.com>

keys.mk
@@ -101,3 +101,4 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     CtsCarrierApiTestCases:cts_uicc_2021.certificate.override
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/voltage-priv/keys/releasekey
+PRODUCT_EXTRA_RECOVERY_KEYS += vendor/voltage-priv/keys/releasekey